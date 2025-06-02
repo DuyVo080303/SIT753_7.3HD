8eeaa9fd0ce8b22fb0a9c11ef2348436eb8723a6
DuyVo080303 <vothaiduy0803@gmail.com>
2025-06-03 00:30:11 +1000

    Update index.js

--- index.js
+++ index.js
@@ -49,6 +49,11 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
 }));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
 app.get('/login', (req, res) => {
     res.render('login', { message: null });
 });
